64e534d68fc3f229f0e31553cee241607b4284a6
alexmwaura <alexmwaura43@gmail.com>
2019-12-02 16:06:56 +0300

    Add resource class

--- watch/admin.py
+++ watch/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from .models import Business,Security,Hospital
+from tastypie.resources import ModelResource
+from tastypie.authentication import Authentication
+from tastypie.authorization import Authorization
+
 # Register your models here.
 
 admin.site.register(Business)
 admin.site.register(Security)
 admin.site.register(Hospital)
+
+
+
+class BusinessResource(ModelResource):
+    """
+     API Facet
+     """
+    class Meta:
+        queryset = Business.objects.all()
+        resource_name = 'business'
+        allowed_methods = ['post', 'get', 'patch', 'delete']
+        authentication = Authentication()
+        authorization = Authorization()
+        always_return_data = True
+
+
+        
