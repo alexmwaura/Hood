f35b078d9e3d751902d90bd4708f10e8e5c12a97
alexmwaura <alexmwaura43@gmail.com>
2019-12-02 16:01:02 +0300

    add url configurations for the view functions

--- watch/urls.py
+++ watch/urls.py
@@ -4,10 +4,16 @@ from watch import views
 urlpatterns = [
     path('', views.Index, name='index'),
     path (r'^search/',views.search_results,name= 'search_results'),
+    # path (r'^search/security',views.search_security,name= 'search_security'),
 
-   
+
+
+    path('business/<int:pk>/',views.BusinessDetailView.as_view(), name ='business-detail'),   
+    path('security/<int:pk>/',views.SecurityDetailView.as_view(), name ='security-detail'),   
+    path('hospital/<int:pk>/',views.HospitalDetailView.as_view(), name ='hospital-detail'),   
    
     path('create/', views.BusinessCreateView.as_view(), name='create_post'),
+    
 ]
 
 
