b3ca6db33c732296d9b15d3d65ad6eb95e743df4
alexmwaura <alexmwaura43@gmail.com>
2019-12-02 16:31:58 +0300

    debug settings file

--- hood/settings.py
+++ hood/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qinuhq$vy!qqi=g&rm6b+f6_biw+pedc-!3m2*2-gdcx*)r*^p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
